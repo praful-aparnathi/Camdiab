9e15cf0a2a10b1c20d99c2f33414fe8e2c0bb937
praful aparnathi <aparnathiprafulk758@gmail.com>
2019-11-17 22:35:08 -0800

    Updates

---
+++ precache-manifest.28b8357766e9e389235bb6fdabd6160b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9d6a854bb87355fc83539b252982e7",
+    "url": "//Camdiab/index.html"
+  },
+  {
+    "revision": "8e2724eb6e713c2b66b0",
+    "url": "//Camdiab/static/css/2.dfe7030c.chunk.css"
+  },
+  {
+    "revision": "5d9f31f514742a000406",
+    "url": "//Camdiab/static/css/main.594f061f.chunk.css"
+  },
+  {
+    "revision": "8e2724eb6e713c2b66b0",
+    "url": "//Camdiab/static/js/2.f7f17063.chunk.js"
+  },
+  {
+    "revision": "5d9f31f514742a000406",
+    "url": "//Camdiab/static/js/main.08e021f6.chunk.js"
+  },
+  {
+    "revision": "5c364315520672fe4f53",
+    "url": "//Camdiab/static/js/runtime-main.1338516e.js"
+  },
+  {
+    "revision": "0c3bf38a9ebf0132709b92be98c2e089",
+    "url": "//Camdiab/static/media/adaptable-ic.0c3bf38a.svg"
+  },
+  {
+    "revision": "2207490c4fe771dcb6ba19884c9219ad",
+    "url": "//Camdiab/static/media/android-ic.2207490c.svg"
+  },
+  {
+    "revision": "523999bbee23cb46311bf5381fe99e2d",
+    "url": "//Camdiab/static/media/clinical-support.523999bb.svg"
+  },
+  {
+    "revision": "91a1db7852bf40253783468b3558c7ca",
+    "url": "//Camdiab/static/media/family-ic.91a1db78.svg"
+  },
+  {
+    "revision": "80c4bc50a2ead87558d4aa40dc5cfba4",
+    "url": "//Camdiab/static/media/file-ic.80c4bc50.svg"
+  },
+  {
+    "revision": "0a3f46e21780d0bf1d4a48727d2e986b",
+    "url": "//Camdiab/static/media/logo.0a3f46e2.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "//Camdiab/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "6340c7a30bc5dfaf76071951d087084d",
+    "url": "//Camdiab/static/media/minus-ic.6340c7a3.svg"
+  },
+  {
+    "revision": "a4b434c11a922d0c0c264c8b837ad665",
+    "url": "//Camdiab/static/media/phone-hero-image.a4b434c1.png"
+  },
+  {
+    "revision": "0f42a26364f700435c5f61701e62512d",
+    "url": "//Camdiab/static/media/plus-ic.0f42a263.svg"
+  },
+  {
+    "revision": "69e3366a275cc915c4b393eb76919e3a",
+    "url": "//Camdiab/static/media/technical-support.69e3366a.svg"
+  },
+  {
+    "revision": "d44c6eea2807b34eb5ac0c6d22a279ab",
+    "url": "//Camdiab/static/media/tested-ic.d44c6eea.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/praful-aparnathi/Camdiab/precache-manifest.049e5ed1c0f8c97625c3327c7d23e128.js"
+  "//Camdiab/precache-manifest.28b8357766e9e389235bb6fdabd6160b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/praful-aparnathi/Camdiab/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//Camdiab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
